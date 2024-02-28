85580e0fd9357d03c0ca30d36e4e29699abef75b
Sergio Vargas <svargasmireles@gmail.com>
2024-02-28 14:41:00 -0600

    iuniun

--- tests/test-1.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://computer-database.gatling.io/computers');
-  await page.getByRole('link', { name: 'Add a new computer' }).click();
-  await page.getByLabel('Computer name').click();
-  await page.getByLabel('Computer name').fill('erert');
-  await page.getByText('Create this computer or Cancel').click();
-});
